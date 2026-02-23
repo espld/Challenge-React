c6b89bee229eea345c0dddf91493c3159605c439
Lucaa espindola <lucasespindola.up@gmail.com>
2026-02-23 13:40:16 -0300

    ultimo

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -23,6 +23,7 @@ function JobItem({ job, candidate }) {
         uuid: candidate.uuid,
         jobId: job.id,
         candidateId: candidate.candidateId,
+        applicationId: candidate.applicationId,
         repoUrl: repoUrl,
       });
 
